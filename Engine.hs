4a44d1327f25e6877d39c1f895de8cb981f096d8
xeeny <xeeny@xeeny.pl>
2017-07-02 11:36:17 +0200

    Reorganization code

--- Engine.hs
+++ Engine.hs
@@ -12,13 +12,13 @@ import           Data.Text.Unsafe              (inlinePerformIO)
 import           Network.Socket
 import           Network.Socket.ByteString     (sendAll)
 import           System.Directory              (doesFileExist)
-import           System.FilePath.Windows       (takeExtension)
 import           System.IO                     (FilePath, appendFile)
 import           System.Time                   (getClockTime)
 import qualified Data.ByteString          as B
 import qualified Data.ByteString.Internal as B (ByteString)
 
 import           Configuration
+import           Mime
 
 data StatusCodes =
       OK
@@ -28,72 +28,6 @@ data StatusCodes =
 data Fields = Fields
     { hostField :: Maybe String
     } deriving (Show)
-
-getMimeType :: FilePath -> Mime
-getMimeType file = case (takeExtension file) of
-    ".bmp"    -> "image/bmp"
-    ".css"    -> "text/css"
-    ".gif"    -> "image/gif"
-    ".htm"    -> "text/html"
-    ".html"   -> "text/html"
-    ".jpg"    -> "image/jpeg"
-    ".jpeg"   -> "image/jpeg"
-    ".js"     -> "application/javascript"
-    ".json"   -> "application/json"
-    ".otf"    -> "font/opentype"
-    ".png"    -> "image/png"
-    ".text"   -> "text/plain"
-    ".aac"    -> "audio/aac"
-    ".abw"    -> "application/x-abiword"
-    ".arc"    -> "application/octet-stream"
-    ".avi"    -> "video/x-msvideo"
-    ".azw"    -> "application/vnd.amazon.ebook"
-    ".bz"     -> "application/x-bzip"
-    ".bz2"    -> "application/x-bzip2"
-    ".csh"    -> "application/x-csh"
-    ".csv"    -> "text/csv"
-    ".doc"    -> "application/msword"
-    ".epub"   -> "application/epub+zip"
-    ".ico"    -> "image/x-icon"
-    ".ics"    -> "text/calendar"
-    ".jar"    -> "application/java-archive"
-    ".mid"    -> "audio/midi"
-    ".midi"   -> "audio/midi"
-    ".mpeg"   -> "video/mpeg"
-    ".mpkg"   -> "application/vnd.apple.installer+xml"
-    ".odp"    -> "application/vnd.oasis.opendocument.presentation"
-    ".ods"    -> "application/vnd.oasis.opendocument.spreadsheet"
-    ".odt"    -> "application/vnd.oasis.opendocument.text"
-    ".oga"    -> "audio/ogg"
-    ".ogv"    -> "video/ogg"
-    ".ogx"    -> "application/ogg"
-    ".pdf"    -> "application/pdf"
-    ".ppt"    -> "application/vnd.ms-powerpoint"
-    ".rar"    -> "application/x-rar-compressed"
-    ".rtf"    -> "application/rtf"
-    ".sh"     -> "application/x-sh"
-    ".svg"    -> "image/svg+xml"
-    ".swf"    -> "application/x-shockwave-flash"
-    ".tar"    -> "application/x-tar"
-    ".tif"    -> "image/tiff"
-    ".tiff"   -> "image/tiff"
-    ".ttf"    -> "font/ttf"
-    ".vsd"    -> "application/vnd.visio"
-    ".wav"    -> "audio/x-wav"
-    ".weba"   -> "audio/webm"
-    ".webm"   -> "video/webm"
-    ".webp"   -> "image/webp"
-    ".woff"   -> "font/woff"
-    ".woff2"  -> "font/woff2"
-    ".xhtml"  -> "application/xhtml+xml"
-    ".xls"    -> "application/vnd.ms-excel"
-    ".xml"    -> "application/xml"
-    ".xul"    -> "application/vnd.mozilla.xul+xml"
-    ".zip"    -> "application/zip"
-    ".3gp"    -> "video/3gpp"
-    ".3g2"    -> "video/3gpp2"
-    ".7z"     -> "application/x-7z-compressed"
-    otherwise -> "application/octet-stream"
 
 lengthFileUnsafe :: FilePath -> Int
 lengthFileUnsafe file = (B.length $ inlinePerformIO $ B.readFile file)
